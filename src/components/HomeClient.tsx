be44527a3e03e5cbe5dfbe103d99024ad95f5e7b
liamemcgrath-sys <liamemcgrath@gmail.com>
2026-06-18 00:49:35 -0400

    Update HomeClient.tsx

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -83,7 +83,7 @@ export default function HomeClient({
   };
 
   return (
-    <div className="w-full">
+    <div className="w-full relative z-10">
 
       {/* ADMIN MODE BANNER */}
       {isAdmin && (
@@ -227,3 +227,4 @@ export default function HomeClient({
     </div>
   );
 }
+
