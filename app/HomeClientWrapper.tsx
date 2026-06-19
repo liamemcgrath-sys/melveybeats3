a3e8cab91b45f41cab3d7ba94f92d361e7740010
liamemcgrath-sys <liamemcgrath@gmail.com>
2026-06-18 20:15:35 -0400

    Create HomeClientWrapper.tsx

---
+++ app/HomeClientWrapper.tsx
@@ -0,0 +1,8 @@
+Module not found: Can't resolve './HomeClientWrapper'
+   6 | export const dynamic = "force-dynamic";
+   7 |
+>  8 | import HomeClient from "./HomeClientWrapper";
+     | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+   9 |
+  10 |
+  11 | export default async function Home() {
