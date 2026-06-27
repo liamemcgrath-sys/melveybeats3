0e3f159417e2d06cf40a00e8fc371b59f5e60cc7
liamemcgrath-sys <liamemcgrath@gmail.com>
2026-06-27 18:16:55 -0400

    Update supabaseServer.ts

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "./database";
+import type { Database } from "./database.types";
 
 let supabaseAdmin: ReturnType<typeof createClient<Database>> | null = null;
 
